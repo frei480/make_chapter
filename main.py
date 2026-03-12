e8f600521b3126e82d60ce11dafea65ff42cf6da
frei480 <frei741@gmail.com>
2026-03-12 23:20:12 +0500

    обновить README.md с описанием возможностей и установки; изменить модель в settings.yaml на qwen/qwen3-vl-4b и скорректировать максимальное количество токенов; улучшить обработку файлов в main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from openai import AsyncOpenAI
 
 from chunks import get_chunks
 from config import cfg
+from prompts import system_prompt, user_prompt
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -15,27 +16,6 @@ input_dir = Path(cfg.main.OUTPUT_DIR)
 files = [f for f in input_dir.rglob(cfg.main.FILE_EXTENSION)]
 
 max_tokens_per_request = cfg.main.OPENAI_MAX_TOKENS
-
-RULES = """
-Проанализируй фрагмент субтитров видео.
-
-Сделай оглавление.
-
-Формат:
-
-MM:SS Название раздела
-
-• мысль
-• мысль
-• мысль
-
-Правила:
-- минимум 3 пункта
-- только ключевые идеи
-- не пересказывай текст дословно
-- используй таймкод начала блока
-- только на русском языке
-"""
 
 
 def get_openai_client() -> AsyncOpenAI:
@@ -53,10 +33,12 @@ async def summarize_chunk(chunk: list[str], i: int) -> str:
         messages=[
             {
                 "role": "system",
-                "content": "Ты создаешь оглавление для видео. Отвечай только на русском языке"
-                + RULES,
+                "content": system_prompt[0],
             },
-            {"role": "user", "content": "\n".join(chunk)},
+            {
+                "role": "user",
+                "content": user_prompt[0].format(subtitles_chunk="\n".join(chunk)),
+            },
         ],
         max_tokens=cfg.main.OPENAI_MAX_TOKENS,
         n=1,
@@ -68,15 +50,19 @@ async def summarize_chunk(chunk: list[str], i: int) -> str:
     return response.choices[0].message.content
 
 
-async def main() -> None:
-    file = files[0]
+async def process_file(file: Path) -> None:
     chunks = get_chunks(file, max_tokens_per_request)
     logger.info(f"File: {file}")
     logger.info(f"Chunks count: {len(chunks)}")
     summarizations = [await summarize_chunk(chunk, i) for i, chunk in enumerate(chunks)]
-    print(summarizations)
     with open(file.with_suffix(".md"), "w", encoding="utf-8") as f:
         f.write("\n\n".join(summarizations))
+
+
+async def main() -> None:
+    file = files[0]
+    for file in files:
+        await process_file(file)
 
 
 if __name__ == "__main__":
